fe752957c36b5782656172e0e47a0a90866b1748
Florian <florianmarkiewicz1@gmail.com>
2022-06-13 16:54:32 +0200

    Dodano szablony requestow

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello")
+import requests
+from datetime import datetime
+
+API_KEY_STOCK = 'MUP3M9P3V54U2WGF'
+
+STOCK_PARAMETERS = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": 'IBM',
+    "outputsize": 'full',
+    "datatype": 'csv',
+    "apikey": API_KEY_STOCK,
+}
+
+STOCK_SEARCH_PARAMETERS = {
+    "function": "SYMBOL_SEARCH",
+    "keywords": 'microsoft',
+    "apikey": API_KEY_STOCK,
+}
+
