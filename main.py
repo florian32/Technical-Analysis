0673e3ca82bcffc4fd6fb87a1de05cbc0de32033
Florian Markiewicz <florianmarkiewicz1@gmail.com>
2023-03-13 09:15:06 +0100

    finished project

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ def home():
         sma = request.form.get("sma")
         current_stock = Stock(stock_symbol, timestamp)
 
-        smoothing = 2
-        window_range = 20
+        smoothing = 3
+        window_range = 1
 
         current_stock.get_min_max(smoothing, window_range)
-        current_stock.find_inverse_head_and_shoulders()
+        current_stock.find_patterns()
         image_dir, patterns_num = current_stock.plot_minmax_patterns(window_range, smoothing, sma, res_lines, formation)
         return render_template("index.html", image_dir=image_dir, pattern_num=patterns_num)
     return render_template("index.html")
--- stock.py
+++ stock.py
@@ -51,54 +51,39 @@ class Stock:
         except KeyError:
             self.max_min = max_min.set_index('day_num')
 
-    def find_inverse_head_and_shoulders(self, stock_symbol=None):
-        # Window range is 5 units
+    def find_patterns(self):
         for i in range(5, len(self.max_min)):
             window = self.max_min.iloc[i - 5:i]
-            if stock_symbol:
-                window = window[stock_symbol]
-
-            # Pattern must play out in less than n units
-            if window.index[-1] - window.index[0] > 100:
-                continue
             a, b, c, d, e = window.iloc[0:5]
-
             # IHS
-            if a < b and c < a and c < e and c < d and e < d and abs(b - d) <= np.mean([b, d]) * 0.02:
+            if b > a > c and c < e and c < d and e < d and abs(b - d) <= np.mean([b, d]) * 0.02 and abs(
+                    a - e) <= np.mean([a, e]) * 0.02:
                 self.patterns['IHS'].append((window.index[0], window.index[-1]))
+            # HS
+            if a > b and c > a and c > e and d < a and e > d and abs(b - d) <= np.mean([b, d]) * 0.02 and abs(
+                    a - e) <= np.mean([a, e]) * 0.02:
+                self.patterns['HS'].append((window.index[0], window.index[-1]))
+            # AT
+            if a > b and b < d < e and abs(a - c) <= np.mean([a, c]) * 0.01 and abs(a - e) <= np.mean(
+                    [a, e]) * 0.01:
+                self.patterns['AT'].append((window.index[0], window.index[-1]))
 
     def find_supp_lines(self):
         supp_lines = []
-        # Window range is 5 units
         for i in range(5, len(self.max_min)):
             window = self.max_min.iloc[i - 5:i]
-
-            # Pattern must play out in less than n units
-            if window.index[-1] - window.index[0] > 100:
-                continue
             a, b, c, d, e = window.iloc[0:5]
-
-            # IHS
             if c < b and c < d < e and b < a:
                 supp_lines.append(c)
-
         return supp_lines
 
     def find_res_lines(self):
         res_lines = []
-        # Window range is 5 units
         for i in range(5, len(self.max_min)):
             window = self.max_min.iloc[i - 5:i]
-
-            # Pattern must play out in less than n units
-            if window.index[-1] - window.index[0] > 100:
-                continue
             a, b, c, d, e = window.iloc[0:5]
-
-            # IHS
             if c > b and c > d > e and b > a:
                 res_lines.append(c)
-
         return res_lines
 
     def plot_minmax_patterns(self, window, ema, sma=False, resistance_levels=False, formations=False):
@@ -107,6 +92,9 @@ class Stock:
             image_timestamp = str(time.time()).split(".")[0]
             image_dir = f"./static/img/no-patterns-{image_timestamp}.png"
             prices.plot(label="Price")
+            plt.title(self.symbol)
+            plt.legend()
+            plt.ylabel("Price [USD]")
             if sma:
                 price_avg = prices.rolling(window=7).mean()
                 price_avg.plot(color='m', linestyle=':', label="SMA")
@@ -128,13 +116,14 @@ class Stock:
                     else:
                         plt.axhline(y=line, color='r', linestyle=':')
                     counter += 1
+                plt.ylabel("Price [USD]")
+                plt.title(self.symbol)
                 plt.legend()
             plt.savefig(image_dir)
             plt.close()
             return image_dir, 0
 
         else:
-            incr = str((self.df.index[1] - self.df.index[0]).seconds / 60)
             max_min = self.max_min
             num_pat = len([x for x in self.patterns.items()][0][1])
             f, axes = plt.subplots(1, 2, figsize=(16, 5))
@@ -145,7 +134,9 @@ class Stock:
                 prices_ = self.df.reset_index()[self.symbol]
                 max_min = self.max_min[self.symbol]
             axes[0].plot(self.df["Close"])
+            axes[0].set_xlabel("Date")
             axes[1].plot(prices_)
+            axes[1].set_xlabel("Day number")
             if sma:
                 price_avg = prices_.rolling(window=7).mean()
                 axes[1].plot(price_avg)
@@ -156,10 +147,10 @@ class Stock:
                     ed = tup[1]
                     axes[1].scatter(max_min.loc[sd:ed].index,
                                     max_min.loc[sd:ed].values,
-                                    s=200, alpha=.3, label="IHS")
+                                    s=200, alpha=.3, label=name)
                     plt.yticks([])
             plt.tight_layout()
-            plt.title('{}: {}: EMA {}, Window {} ({} patterns)'.format(self.symbol, incr, ema, window, num_pat))
+            plt.title(self.symbol)
             if sma:
                 price_avg = prices_.rolling(window=7).mean()
                 price_avg.plot(color='m', linestyle=':', label="SMA")
@@ -181,7 +172,9 @@ class Stock:
                         plt.axhline(y=line, color='r', linestyle=':')
                     counter += 1
 
+            plt.title(self.symbol)
             plt.legend()
+            plt.ylabel("Price [USD]")
             image_timestamp = str(time.time()).split(".")[0]
             image_dir = f"./static/img/{image_timestamp}.png"
             plt.savefig(image_dir)
