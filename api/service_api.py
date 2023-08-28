7ca4847cff17859d2c9688bcbb8da2ab13c65370
Florian Markiewicz <florianmarkiewicz1@gmail.com>
2023-08-28 14:08:54 +0200

    added endpoint

--- api/service_api.py
+++ api/service_api.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from flask import Blueprint
 from flask import current_app as app
 from typing import Dict, Tuple
@@ -6,6 +7,7 @@ from flask import request, session
 from flask_restful import Api, Resource
 from . import QueryParamsParseMixin
 from config import config
+from utils.stock import Stock
 
 blueprint = Blueprint("api", __name__, url_prefix="/api")
 api = Api(blueprint)
@@ -27,12 +29,25 @@ class NewsListResource(QueryParamsParseMixin, Resource):
         return response, 200
 
 
-@api.resource("/analysis/<symbol>")
+@api.resource("/analysis/<symbol>/<image_path>")
 class StockAnalysisResource(QueryParamsParseMixin, Resource):
     query_params = {
         "timestamp": lambda req: str(req.args.get("timestamp")),
         "analysis_types": lambda req: str(req.args.get("analysis_types", 3)).split(','),
     }
 
-    def post(self) -> Tuple[Dict, int]:
-        pass
+    def post(self, symbol) -> Tuple[Dict, int]:
+        stock = Stock(symbol, self._request_query_parameters["timestamp"])
+        stock.get_min_max()
+        stock.find_patterns()
+        image_dir, patterns_num = stock.plot_minmax_patterns(sma=self._request_query_parameters["sma"] ,
+                                                             resistance_levels=self._request_query_parameters["res"],
+                                                             formations=self._request_query_parameters["formations"])
+        return {"image_dir": image_dir, "patterns_num": patterns_num}, 200
+
+    def delete(self, image_path) -> Tuple[Dict, int]:
+        if image_path and os.path.exists(image_path):
+            os.remove(image_path)
+            return {"message": "Image deleted successfully."}, 200
+        else:
+            return {"message": "Image not found or unable to delete."}, 404
