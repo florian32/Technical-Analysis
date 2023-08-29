f236c89a4965ea48b2a418a36aeec3c49256a320
Florian Markiewicz <florianmarkiewicz1@gmail.com>
2023-08-29 15:55:08 +0200

    added new var

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 class Config:
     MARKETAUX_ENDPOINT: str = os.getenv("MARKETAUX_ENDPOINT")
     MARKETAUX_TOKEN: str = os.getenv("MARKETAUX_TOKEN")
+    SQLALCHEMY_DATABASE_URI: str = os.getenv("SQLALCHEMY_DATABASE_URI")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     SMOOTHING = 3
     WINDOW_RANGE = 1
