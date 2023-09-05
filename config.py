ad02f8fc5b14283c81157d6f4cec7d4cf7d3a1f1
Florian Markiewicz <florianmarkiewicz1@gmail.com>
2023-09-05 10:48:27 +0200

    added env vars

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ import os
 class Config:
     MARKETAUX_ENDPOINT: str = os.getenv("MARKETAUX_ENDPOINT")
     MARKETAUX_TOKEN: str = os.getenv("MARKETAUX_TOKEN")
+    HASH_METHOD: str = os.getenv("HASH_METHOD")
+    SALT_LENGTH: int = os.getenv("SALT_LENGTH")
     SQLALCHEMY_DATABASE_URI: str = os.getenv("SQLALCHEMY_DATABASE_URI")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     SMOOTHING = 3
