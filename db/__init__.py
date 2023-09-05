3e3109c2b89043c67c64adca7d12f753098ff0c2
Florian Markiewicz <florianmarkiewicz1@gmail.com>
2023-09-05 10:48:21 +0200

    added fields

--- db/__init__.py
+++ db/__init__.py
@@ -20,6 +20,7 @@ class Search(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     stock = db.Column(db.String(15), nullable=False)
     timestamp = db.Column(db.String(3), nullable=True)
-    analysis_types = db.Column(db.String(20), nullable=True)
-
+    sma = db.Column(db.Bool, nullable=False)
+    res = db.Column(db.Bool, nullable=False)
+    formations = db.Column(db.Bool, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
