3617e9b31edac5b2abadf33f41a2f998cb6bdf3e
Florian Markiewicz <florianmarkiewicz1@gmail.com>
2023-09-05 14:14:05 +0200

    changed db structure

--- db/__init__.py
+++ db/__init__.py
@@ -3,9 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+app = Flask(__name__, instance_relative_config=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 
 
 class User(db.Model):
@@ -20,7 +23,7 @@ class Search(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     stock = db.Column(db.String(15), nullable=False)
     timestamp = db.Column(db.String(3), nullable=True)
-    sma = db.Column(db.Bool, nullable=False)
-    res = db.Column(db.Bool, nullable=False)
-    formations = db.Column(db.Bool, nullable=False)
+    sma = db.Column(db.Boolean, nullable=False)
+    res = db.Column(db.Boolean, nullable=False)
+    formations = db.Column(db.Boolean, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
