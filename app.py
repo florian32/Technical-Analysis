7ace3318aeedcd64c494f6e98b3c5e9e33564504
Florian Markiewicz <florianmarkiewicz1@gmail.com>
2023-09-26 14:02:25 +0200

    moved db init here

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
 from werkzeug.exceptions import HTTPException, default_exceptions
+from sqlalchemy.exc import OperationalError
 from api import(
     service_api,
     user_api
 )
+from db import db
 from config import config
 
 
@@ -15,11 +17,19 @@ def make_json_error(ex: Exception) -> jsonify:
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Initialize the SQLAlchemy extension
+    db.init_app(app)
 
     with app.app_context():
         app.secret_key = config.SECRET_KEY
         app.config.from_object(config)
-
+        try:
+            db.create_all()
+        except OperationalError as e:
+            print(f"Error {e}")
     for api in [service_api, user_api]:
         app.register_blueprint(api.blueprint)
 
